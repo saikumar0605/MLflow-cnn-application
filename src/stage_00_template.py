05d36dfb1a9186e5cd8980db3c8293727f2b784c
saikumar0605 <123saikumarreddy@gmail.com>
2023-07-04 13:08:36 +0530

    made changes in code {data loading}

--- src/stage_00_template.py
+++ src/stage_00_template.py
@@ -1,10 +1,13 @@
 import argparse
+import sys
+sys.path.insert(0, '../src')
 import os
 import shutil
 from tqdm import tqdm
 import logging
-from src.utils.common import read_yaml, create_directories
+from src.utils.common import read_yaml, create_directories, save_json
 import random
+import urllib.request as req
 
 
 STAGE = "STAGE_NAME" ## <<< change stage name 
@@ -20,6 +23,21 @@ logging.basicConfig(
 def main(config_path, params_path):
     ## read config files
     config = read_yaml(config_path)
+    URL = config["data"]["URL"]
+    local_dir = config["data"]["local_dir"]
+    create_directories([local_dir])
+
+    data_file = config["data"]["data_file"]
+    data_file_path = os.path.join(local_dir, data_file)
+
+    
+    if not os.path.exists(data_file_path):
+        logging.info("Download started...")           
+        filename, headers = req.urlretrieve(URL, data_file_path)
+        logging.info(f"filename: {filename} created with info \n{headers}")
+    else:
+        logging.info(f"filename:{data_file} already exists")
+
     params = read_yaml(params_path)
     pass
 
