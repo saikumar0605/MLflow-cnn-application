a114e601b28b588a2aa57517c38cf3e824641957
saikumar0605 <123saikumarreddy@gmail.com>
2023-07-07 18:48:37 +0530

    mlflow run fail

---
+++ src/main.py
@@ -0,0 +1,36 @@
+import mlflow
+import argparse
+import os
+import shutil
+from tqdm import tqdm
+import logging
+from src.utils.common import read_yaml, create_directories
+import random
+#this is a default template. to use and create complete end to end ml flow project.
+
+STAGE = "Main" ## <<< change stage name 
+logging.basicConfig(
+    filename=os.path.join("logs", 'running_logs.log'), 
+    level=logging.INFO, 
+    format="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s",
+    filemode="a"
+    )
+
+
+#now in mail.py we we use mlflow to run it and work.
+def main():
+    with mlflow.start_run() as run:
+        mlflow.run(".", "get_data", use_conda=False)
+        mlflow.run(".", "base_model_creation", use_conda=False)
+
+
+if __name__ == '__main__':
+
+    try:
+        logging.info("\n********************")
+        logging.info(f">>>>> stage {STAGE} started <<<<<")
+        main()
+        logging.info(f">>>>> stage {STAGE} completed!<<<<<\n")
+    except Exception as e:
+        logging.exception(e)
+        raise e
